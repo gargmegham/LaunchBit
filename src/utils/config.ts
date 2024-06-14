e1656689a7ea0ef3d57dc4523e5db825ea699755
Megham Garg <95271253+gargmegham@users.noreply.github.com>
2024-06-14 20:27:35 +0530

    chore: update linkedin link

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -8,12 +8,7 @@ const config = {
     keywords:
         "website user experience testing, data analytics, ai development, website user experience tracking, data science, website user experience research, website user interface monitoring, website speed optimization, website user experience support, website user experience optimization, website user experience development, website user interface management, software development, website performance optimization, website user experience analysis, website tracking, website migration, website monitoring, website analytics, website user interface support, website user experience management, website user interface tracking, website management, website user experience maintenance, website hosting, website development, web development, website redesign, machine learning, website seo, website user experience, website user experience design, website user interface maintenance, website accessibility, website user interface optimization, website testing, website optimization, website user, website design, agency, website user interface testing, web design, website user interface design, website user interface research, website maintenance, website user interface development, website user interface analysis, website support, website user interface, website user experience monitoring, website security, website conversion rate optimization",
     socials: {
-        twitter: "https://twitter.com/launchbit",
-        linkedin: "https://www.linkedin.com/company/launchbit",
-        instagram: "https://www.instagram.com/launchbit",
-        github: "https://www.github.com/gargmegham/launchbit",
-        telegram: "https://t.me/launchbit",
-        youtube: "https://www.youtube.com/@launchbit",
+        linkedin: "https://www.linkedin.com/company/launch-bit",
     },
 } as ConfigProps
 

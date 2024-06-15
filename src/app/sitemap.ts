c40faae4d7ccd17e9f534b3697a4339cec9c8c76
Megham Garg <gargmegham3@gmail.com>
2024-06-15 10:35:55 +0530

    chore: Update sitemap.ts with privacy policy and terms of service URLs

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -11,5 +11,17 @@ export default function sitemap(): MetadataRoute.Sitemap {
             changeFrequency: "yearly",
             priority: 1,
         },
+        {
+            url: `https://${config.domainName}/tos`,
+            lastModified: new Date(),
+            changeFrequency: "yearly",
+            priority: 0.5,
+        },
+        {
+            url: `https://${config.domainName}/privacy-policy`,
+            lastModified: new Date(),
+            changeFrequency: "yearly",
+            priority: 0.5,
+        },
     ]
 }
